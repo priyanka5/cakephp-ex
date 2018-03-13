3d161b33718463969bd053596b79a307615664b6
priyash <priyanka.e.gupta@accenture.com>
2018-03-13 18:09:01 +0530

    Update database.php

--- app/Config/database.php
+++ app/Config/database.php
@@ -93,8 +93,8 @@ class DATABASE_CONFIG {
 
         $datasource = 'Database/'.ucfirst(getenv('DATABASE_ENGINE'));
 
-        $this->default['host']       = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_HOST");
-        $this->default['port']       = getenv(strtoupper(getenv("DATABASE_SERVICE_NAME"))."_SERVICE_PORT");
+        $this->default['host']       = localhost;
+        $this->default['port']       = 3306;
         $this->default['login']      = getenv("DATABASE_USER");
         $this->default['password']   = getenv("DATABASE_PASSWORD");
         $this->default['database']   = getenv("DATABASE_NAME");
